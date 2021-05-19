1222c18a60eec11f75bb1b3daa5f7d38b67a1244
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2021-05-19 15:15:27 +0200

    Allow directly saving to HDF5

--- augmentation/creator.py
+++ augmentation/creator.py
@@ -8,6 +8,8 @@ import numpy as np
 import os
 import pandas as pd
 import json
+import datetime
+import h5py
 from time import time
 import matplotlib.pyplot as plt
 
@@ -51,6 +53,9 @@ class Creator:
         None.
 
         """
+        self.timestamp = datetime.datetime.now().strftime("%Y%m%d")
+        self.name =  self.timestamp + "_" + params["run_name"]
+        
         default_param_filepath = os.path.join(
             os.path.dirname(os.path.abspath(__file__)), "default_params.json"
         )
@@ -178,6 +183,13 @@ class Creator:
             self.augmentation_matrix[
                 i, self.no_of_linear_params + 1 :
             ] = self.select_sim_params(key)
+                
+            print(
+                "Random parameters: "
+                + str(i + 1)
+                + "/"
+                + str(self.no_of_simulations)
+            )
 
     def select_reference_set(self):
         """
@@ -578,7 +590,7 @@ class Creator:
             )
             plt.show()
 
-    def to_file(self, filepath, filetype, how="full"):
+    def to_file(self, filetypes):
         """
         Create file from the dataframe of simulated spectra.
 
@@ -588,30 +600,23 @@ class Creator:
             Filepath of the output file.
         filetype : str
             Options: 'excel', 'json', 'txt', 'pickle'
-        how : str, optional
-            if how == 'full':
-                All columns of the dataframe are saved.
-            if how == 'reduced':
-                Only the  columns x, y, and label are saved.
-            The default is 'full'.
         Returns
         -------
         None.
 
         """
-        filetypes = ["excel", "json", "pickle"]
+        filepath = os.path.join(*[self.params["output_datafolder"],
+                                  self.name])
+                                  
+        valid_filetypes = ["excel", "json", "pickle", "hdf5"]
 
-        if filetype not in filetypes:
-            print("Saving was not successful. Choose a valid filetype!")
-        else:
-            print("Data was saved.")
+        for filetype in filetypes:
+            if filetype not in valid_filetypes:
+                print("Saving was not successful. Choose a valid filetype!")
+            else:
+                self._save_to_file(self.df, filepath, filetype)
+                print("Data was saved.")
 
-        if how == "full":
-            df = self.df
-        elif how == "reduced":
-            df = self.reduced_df
-
-        self._save_to_file(df, filepath, filetype)
 
     def _save_to_file(self, df, filename, filetype):
         """
@@ -647,6 +652,125 @@ class Creator:
             file = filename + ".pkl"
             with open(file, "wb") as pickle_file:
                 df.to_pickle(pickle_file)
+        
+        if filetype == "hdf5":
+            self.hdf5_filepath = filename + ".h5"
+            
+            hdf5_data = self.prepare_hdf5(self.df)
+            
+            with h5py.File(self.hdf5_filepath, "w") as hf:
+                for key, value in hdf5_data.items():
+                    try:
+                        hf.create_dataset(
+                            key, 
+                            data=value, 
+                            compression="gzip", 
+                            chunks=True)
+                    except TypeError:
+                        value = np.array(value, dtype=object)
+                        string_dt = h5py.special_dtype(vlen=str)
+                        hf.create_dataset(
+                            key,
+                            data=value,
+                            dtype=string_dt,
+                            compression="gzip",
+                            chunks=True)
+                    print("Saved " + key + " to HDF5 file.")
+
+            
+    def prepare_hdf5(self, df):            
+        X = []
+        y = []
+        shiftx = []
+        noise = []
+        FWHM = []
+        scatterer = []
+        distance = []
+        pressure = []
+    
+        energies = df["x"][0]
+        for index, row in df.iterrows():
+            X_one = row["y"]
+            y_one = row["label"]
+            shiftx_one = row["shift_x"]
+            noise_one = row["noise"]
+            FWHM_one = row["FWHM"]
+            scatterer_name = row["scatterer"]
+            scatterers = {"He": 0, "H2": 1, "N2": 2, "O2": 3}
+            try:
+                scatterer_one = scatterers[scatterer_name]
+            except KeyError:
+                scatterer_one = float("NAN")
+            distance_one = row["distance"]
+            pressure_one = row["pressure"]
+            
+            X.append(X_one)
+            y.append(y_one)
+            shiftx.append(shiftx_one)
+            noise.append(noise_one)
+            FWHM.append(FWHM_one)
+            scatterer.append(scatterer_one)
+            distance.append(distance_one)
+            pressure.append(pressure_one)
+            
+            print(
+                "Prepare HDF5 upload: "
+                + str(index)
+                + "/"
+                + str(df.shape[0])
+            )
+    
+        X = np.array(X)
+        X = np.reshape(X, (X.shape[0], X.shape[1], 1))
+        y = self._one_hot_encode(y)
+    
+        shiftx = np.reshape(np.array(shiftx), (-1, 1))
+        noise = np.reshape(np.array(noise), (-1, 1))
+        FWHM = np.reshape(np.array(FWHM), (-1, 1))
+        scatterer = np.reshape(np.array(scatterer), (-1, 1))
+        distance = np.reshape(np.array(distance), (-1, 1))
+        pressure = np.reshape(np.array(pressure), (-1, 1))
+        
+        return {"X": X,
+                "y": y,
+                "shiftx": shiftx,
+                "noise": noise,
+                "FWHM": FWHM,
+                "scatterer": scatterer,
+                "distance": distance,
+                "pressure": pressure,
+                "energies": energies,
+                "labels": self.labels}
+    
+    def _one_hot_encode(self, y):
+        """
+        One-hot encode the labels.
+        As an example, if the label of a spectrum is Fe metal = 1 and all 
+        oxides = 0, then the output will be np.array([1,0,0,0],1).
+    
+        Parameters
+        ----------
+        y : list
+            List of label strings.
+    
+        Returns
+        -------
+        new_labels : arr
+            One-hot encoded labels.
+    
+        """
+        new_labels = np.zeros((len(y), len(self.labels)))
+    
+        for i, d in enumerate(y):
+            for species, value in d.items():
+                number = self.labels.index(species)
+                new_labels[i, number] = value
+    
+        return new_labels
+    
+    def save_run_params(self):
+        pass
+        
 
 
 def calculate_runtime(start, end):
@@ -679,26 +803,33 @@ def calculate_runtime(start, end):
 
 #%%
 if __name__ == "__main__":
-    t0 = time()
-    init_param_folder = r"C:\Users\pielsticker\Simulations"
-    init_param_filepath = os.path.join(init_param_folder, "init_params.json")
+    runtimes = {}
+    
+    init_param_filepath = r"C:\Users\pielsticker\Simulations\init_params.json"
     with open(init_param_filepath, "r") as param_file:
         params = json.load(param_file)
 
-    creator = Creator(params=params)
+    t0 = time()
+    creator = Creator(params)
     creator.run()
     creator.plot_random(10)
-    #     output_datafolder = params["output_datafolder"]
-    #     output_filepath = os.path.join(
-    #         output_datafolder, "multiple_species_gas_phase"
-    #     )
-    # =============================================================================
-    # =============================================================================
-    #     creator.to_file(filepath = output_filepath,
-    #                     filetype = 'json',
-    #                     how = 'full')
-    # =============================================================================
+    creator.to_file(filetypes = ['hdf5'])
     t1 = time()
     runtime = calculate_runtime(t0, t1)
     print(f"Runtime: {runtime}.")
-    del (t0, t1, runtime)
+    
+    # Test new file.
+    t0 = time()
+    with h5py.File(creator.hdf5_filepath, "r") as hf:
+        size = hf["X"].shape
+        X_h5 = hf["X"][:4000, :, :]
+        y_h5 = hf["y"][:4000, :]
+        shiftx_h5 = hf["shiftx"][:4000, :]
+        noise_h5 = hf["noise"][:4000, :]
+        fwhm_h5 = hf["FWHM"][:4000, :]
+        energies_h5 = hf["energies"][:]
+        labels_h5 = [str(label) for label in hf["labels"][:]]
+        scatterers_h5 = hf["scatterer"][:4000, :]
+
+
+
